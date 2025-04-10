386e294854dc4661adbb40497a999efcd0cfd76c
Michael Borromeo <mbborromeo@gmail.com>
2025-04-09 23:36:11 -0700

    Refactor Movies: response.ok check

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -20,15 +20,41 @@ const Movies = () => {
   useEffect( 
     () => {
       fetch(`https://api.themoviedb.org/3/account/${TMDB_ACCOUNT_ID}/favorite/movies?language=en-US&page=1&sort_by=created_at.asc`, options)
-        .then(res => res.json())
-        .then( 
-          (res) => {
-            if (res.results) {
-              setMovies(res.results);
+        .then( (response) => {
+            // https://dev.to/dionarodrigues/fetch-api-do-you-really-know-how-to-handle-errors-2gj0
+
+            if (response.ok) {
+              console.log('Promise resolved and HTTP status is successful');
+              return response.json();
+            } else {
+              console.error('Promise resolved but HTTP status failed');
+
+              if (response.status === 404) {
+                throw new Error('404, Not found');
+              }
+
+              if (response.status === 500) {
+                throw new Error('500, internal server error');
+              }
+
+              // For any other server error
+              throw new Error(response.status);
             }
           }
         )
-        .catch(err => console.error(err));
+        .then( 
+          (response) => {
+            if (response.results) {
+              setMovies(response.results);
+            }
+          }
+        )
+        .catch(
+          (err) => {
+            // Network or CORS issues
+            console.error('Promise rejected. Error:', err);
+          }
+        );
     }, 
     [options]
   );  
