e8a6138c5505fdf1b54274141cc89776971c517a
Michael Borromeo <mbborromeo@gmail.com>
2025-04-16 15:59:21 -0700

    prettier format fix

--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -46,14 +46,14 @@ const Trailer = ({ id }) => {
             {errorMessage && <ErrorFeedback message={errorMessage} />}
 
             {!loading && !errorMessage && trailer && (
-                <iframe 
-                    id="ytplayer" 
-                    type="text/html" 
-                    width="640" 
+                <iframe
+                    id="ytplayer"
+                    type="text/html"
+                    width="640"
                     height="360"
                     src={`https://www.youtube.com/embed/${trailer.key}?autoplay=1`} // &origin=http://example.com
                     frameborder="0"
-                ></iframe> 
+                ></iframe>
             )}
         </>
     );
