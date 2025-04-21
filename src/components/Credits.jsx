a686041db3350ed25ae4f65420afb10feb25a4d3
Michael Borromeo <mbborromeo@gmail.com>
2025-04-21 14:26:20 -0700

    format fix

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -15,14 +15,18 @@ const Credits = ({
 
     const numberOfActors = actors && actors.length > 0 ? actors.length : 0;
 
-    const maximumActors = numberOfActors > 3 && actorsDisplayMaxThree ? 3 : numberOfActors;
+    const maximumActors =
+        numberOfActors > 3 && actorsDisplayMaxThree ? 3 : numberOfActors;
 
     return (
         <div className="credits">
             <div className="credits-wrapper">
                 <b>{`Director${numberOfDirectors > 1 ? 's' : ''}:`} </b>
-                <div className={`row persons-wrapper${ !showActorsPic ? ' no-photo' : '' }`}>
-                    {directors && numberOfDirectors > 0 &&
+                <div
+                    className={`row persons-wrapper${!showActorsPic ? ' no-photo' : ''}`}
+                >
+                    {directors &&
+                        numberOfDirectors > 0 &&
                         directors.map((director) => (
                             <Person
                                 key={director.id}
@@ -38,22 +42,24 @@ const Credits = ({
 
             <div className="credits-wrapper">
                 <b>Actors: </b>
-                <div className={`row persons-wrapper${ !showActorsPic ? ' no-photo' : '' }`}>
-                    {actors && numberOfActors > 0 && 
+                <div
+                    className={`row persons-wrapper${!showActorsPic ? ' no-photo' : ''}`}
+                >
+                    {actors &&
+                        numberOfActors > 0 &&
                         actors.slice(0, maximumActors).map((actor) => (
                             <Person
                                 key={actor.id}
                                 person={actor}
                                 movieId={movieId}
                                 {...(showActorsPic && {
-                                    showPic: true,
+                                    showPic: true
                                 })}
                                 {...(displayLinks && {
-                                    displayLinks: true,
+                                    displayLinks: true
                                 })}
                             />
-                        ))
-                    }
+                        ))}
                 </div>
             </div>
         </div>
