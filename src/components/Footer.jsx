50e89196352bfd217fb19d7ac5981841ace9d0df
Michael Borromeo <mbborromeo@gmail.com>
2025-05-02 18:56:08 -0700

    pin Footer bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 const Footer = () => {
     return (
         <footer>
-            This website uses{' '}
-            <a href="https://www.themoviedb.org" target="_blank">
-                TMDB
-            </a>{' '}
-            and the TMDB APIs but is not endorsed, certified, or otherwise
-            approved by TMDB.
+            <div>
+                This website uses{' '}
+                <a href="https://www.themoviedb.org" target="_blank">
+                    TMDB
+                </a>{' '}
+                and the TMDB APIs but is not endorsed, certified, or otherwise
+                approved by TMDB.
+            </div>
         </footer>
     );
 };
