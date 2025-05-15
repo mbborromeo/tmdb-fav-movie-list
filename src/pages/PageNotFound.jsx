4abf6eafddd7f3e08bf2a3ea011d30a352aab5eb
Michael Borromeo <mbborromeo@gmail.com>
2025-05-15 12:54:38 -0700

    edit text 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const PageNotFound = () => (
     <div>
-        <h1>404 - Page Not Found!</h1>
+        <h1>Page Not Found - 404 Error</h1>
         <Link to="/">Go to Home</Link>
     </div>
 );
