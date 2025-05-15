360df5560b89f5e5f025551e9218fda29ccca5fa
Michael Borromeo <mbborromeo@gmail.com>
2025-05-15 12:27:16 -0700

    SPA component: 404 PageNotFound

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Movies from './pages/Movies.jsx';
 import Movie from './pages/Movie.jsx';
 import Person from './pages/Person.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
                 <Route exact path="/" element={<Movies />} />
                 <Route path="/movie/:id" element={<Movie />} />
                 <Route path="/person/:id" element={<Person />} />
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </>
     );
