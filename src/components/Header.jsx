99b68046105648509f0c4fb924ae7e773046d2e4
Michael Borromeo <mbborromeo@gmail.com>
2025-05-16 01:06:38 -0700

    lint fix + rebuild

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 const Header = () => {
-    return (
-        <h2>Mike's movie picks: 80's &amp; 90's</h2>
-    );
+    return <h2>Mike's movie picks: 80's &amp; 90's</h2>;
 };
 
 export default Header;
