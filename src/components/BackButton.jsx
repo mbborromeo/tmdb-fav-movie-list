f49346b6e9bdfeca6bd15d7a60deb1d4db4ec941
Michael Borromeo <mbborromeo@gmail.com>
2025-05-19 13:02:02 -0700

    WIP: BackButton with useLocation to make re-render

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,7 +1,19 @@
-import { useNavigate } from 'react-router-dom';
+// import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // , useLocation
 
 const BackButton = () => {
     const navigate = useNavigate();
+    // const location = useLocation();
+
+    console.log('BackButon: window.history.state', window.history.state);
+    // console.log('location', location);
+
+    // useEffect( 
+    //     () => {
+    //         console.log('BackButton has re-rendered upon location change');
+    //     }, 
+    //     [location]
+    // );
 
     return (
         window.history.state.idx >= 1 && (
