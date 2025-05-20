e69fe41f5b9376bd3d550fa5c0203c93c50f60e8
Michael Borromeo <mbborromeo@gmail.com>
2025-05-20 00:07:46 -0700

    format + build

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -6,16 +6,12 @@ const BackButton = () => {
 
     const canGoBack = location.key !== 'default'; // window.history.state?.idx > 0
 
-    return (
-        canGoBack ? (
-            <button onClick={() => navigate(-1)} className="btn back">
-                &laquo;Back
-            </button>
-        ) : (
-            <Link to="/">
-                &laquo;Back to Movies
-            </Link>
-        )
+    return canGoBack ? (
+        <button onClick={() => navigate(-1)} className="btn back">
+            &laquo;Back
+        </button>
+    ) : (
+        <Link to="/">&laquo;Back to Movies</Link>
     );
 };
 
